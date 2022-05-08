a57432d07d510cf75867711638451dc93545ae31
Refael <refaelbenzvi24@gmail.com>
2022-05-08 21:04:00 +0300

    allowed cors on production for socket.io

--- src/config/socketIo.js
+++ src/config/socketIo.js
@@ -1,13 +1,10 @@
 import SocketIo from "socket.io"
-import {env, clientUrl} from "./vars"
 
 const config = {}
 
-if (env === "development") {
-    config["cors"] = {
-        origin:  "*",
-        methods: ["GET", "POST"]
-    }
+config["cors"] = {
+    origin:  "*",
+    methods: ["GET", "POST"]
 }
 
 export default SocketIo(config)
